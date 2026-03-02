7811dc0f6ed5e01de45d9bce2e12b16e4914e897
David Brown <dbrown92700@gmail.com>
2026-03-01 23:32:22 -0500

    Fixed errors

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SLA Reporting Tool
+# Cisco SDWAN SLA Reporting Tool
 
 ## Overview
 The goal of this tool is to view historical information around SDWAN Tunnel SLA and what traffic
@@ -9,6 +9,16 @@ time period.  For multi-BFD tunnels the report displays the percentage of time t
 is removed from a tunnel for each time period.
 
 ![img.png](img.png)
+
+The script will also generate a CSV. Here's an example of using Excel and applying conditional formatting with the default
+"high is bad" format and filtering down to compare Queue0 and Queue2 on 3 colors on a specific tunnel
+with a report run across 3 days (72 hours).
+
+![img_1.png](img_1.png)
+
+Here's another report on the same date with 15 minute intervals on the middle day shown above.
+
+![img_3.png](img_3.png)
 
 ## Installation
 > git clone https://github.com/dbrown92700/SDWAN_SLA \
--- sla_report.py
+++ sla_report.py
@@ -4,35 +4,135 @@ from datetime import datetime
 
 # 10.33.205.24
 
-time_range = 3
-last_n = True
-period = 900
-entered_time = '9:00 2/25/2026'
-start_time = datetime.strptime(entered_time, '%H:%M %m/%d/%Y')
+gmt_adjust = 5 * 3600
 
+def generate_output(tunnels, print_screen=True, csv_file=None):
 
-def print_queues(tunnels):
+    sep = ' '
+    if csv_file:
+        f = open(csv_file, 'w', newline='')
+        sep = ','
+
     times = ''
     for t in range(int(time_range * 3600 / period)):
         time = datetime.fromtimestamp(start_time + period * t)
-        times += f'{time.hour:02}:{time.minute:02} '
+        times += f'{time.month:02}/{time.day:02}-{time.hour:02}:{time.minute:02} '
+    if csv_file:
+        f.write(f'Color,Remote sys-ip,Queue,{times.replace(" ",",")}\n')
+
     for c in tunnels:
-        print(f'{"Color":16}{"Remote sys-ip":16}{"Queue":7} {times}')
+        if print_screen:
+            print(f'{"Color":16}{"Remote sys-ip":16}{"Queue":7} {times}')
         for t in tunnels[c]:
             for q in queues:
-                stats = ''
+                p_stats = ''
+                f_stats = ''
                 for h in tunnels[c][t][1][q]:
+                    # If time slot has full time (period seconds), substitute '-' for 0 for printed output
                     if h == period:
-                        stats += '    - '
+                        if csv_file:
+                            f_stats += ','
+                        if print_screen:
+                            p_stats += '          - '
                     else:
-                        stats += f'{100 - int(h * 100 / period):5} '
-                print(f'{c:16}{t:16}{q:7} {stats}')
+                        # Calculate percent downtime
+                        f_stats += f'{100 - int(h * 100 / period)},'
+                        p_stats += f'{100 - int(h * 100 / period):11} '
+                if print_screen:
+                    print(f'{c:16}{t:16}{q:7} {p_stats}')
+                if csv_file:
+                    f.write(f'{c},{t},{q},{f_stats}\n')
+    if not print_screen:
+        f.close()
 
 def print_tunnel_events(color, rip):
     for x in sla_events:
         if x['event']['local-color'] == color and x['event']['remote-system-ip'] == rip:
             print(datetime.fromtimestamp(int(x['statcycletime'] / 1000)))
             print(x['event'])
+
+def calculate_downtime(start_time, time_range, period, sla_events, tunnels, queues):
+    # Set up tunnels data structure
+    for color in tunnels:
+        for rip in tunnels[color]:
+            q = [0, {q: [0 for x in range(int(time_range * 3600 / period))] for q in queues}]
+            tunnels[color][rip] = q
+
+    # Parse events
+    for event in sla_events:
+
+        e = event['event']
+        old_q = e['old-sla-classes'].split(', ')
+        new_q = e['sla-classes'].split(', ')
+
+        event_tunnel = tunnels[e['local-color']][e['remote-system-ip']]
+
+        event_time = int(event['entry_time'] / 1000) - start_time
+        if event_time == time_range * 3600:
+            event_time -= 1
+        event_hour = int(event_time / period)
+        event_seconds = event_time % period
+        tunnel_last_event_time = event_tunnel[0]
+        tunnel_last_event_hour = int(tunnel_last_event_time / period)
+        event_tunnel[0] = event_time
+
+        if tunnel_last_event_time == 0:
+            # First event on this tunnel
+            if old_q != ['None']:
+                for queue in old_q:
+                    # for queues that were in SLA
+                    time_list = event_tunnel[1][queue]
+                    for t in range(event_hour):
+                        # fill in all fully elapsed hours
+                        time_list[t] = period
+                    # put remaining time in next hour
+                    time_list[event_hour] = event_seconds
+            event_tunnel.append(new_q)
+        else:
+            # Later events on this tunnel
+            if old_q != ['None']:
+                time_dist = [0 for x in range(int(time_range * 3600 / period))]
+                # If queue was in SLA, credit it for elapsed time
+                if tunnel_last_event_hour == event_hour:
+                    time_dist[event_hour] = event_time - tunnel_last_event_time
+                else:
+                    time_dist[tunnel_last_event_hour] = (tunnel_last_event_hour + 1) * period - tunnel_last_event_time
+                    for t in range(tunnel_last_event_hour + 1, event_hour):
+                        # Fill in all fully elapsed hours
+                        time_dist[t] = period
+                    # Put remaining time in next hour
+                    time_dist[event_hour] = event_seconds
+                for queue in old_q:
+                    for x in range(int(time_range * 3600 / period)):
+                        event_tunnel[1][queue][x] += time_dist[x]
+            event_tunnel[2] = new_q
+
+        # if e['local-color'] == 'private2' and e['remote-system-ip'] == '10.46.18.56':
+        #     print(datetime.fromtimestamp(int(event['statcycletime'] / 1000)), event_time - tunnel_last_event_time)
+        #     print(time_dist)
+        #     print(f'{e['old-sla-classes']}\n\n     {event_tunnel}\n')
+
+    # Fill in remaining time for Queues in SLA on last event
+    for c in tunnels:
+        for t in tunnels[c]:
+            last_event_time = tunnels[c][t][0]
+            last_event_hour = int(last_event_time / period)
+            # Calculate remainder seconds.  If event happened on an hour boundry seconds should be 3600.
+            last_event_seconds = (time_range * 3600 - last_event_time) % period or period
+            try:
+                # Get queues in SLA on last time period
+                new_q = tunnels[c][t][2]
+            except IndexError:
+                # If there's no entry, there were no events. Assume all queues were in SLA
+                new_q = queues
+            if new_q != ['None']:
+                for q in new_q:
+                    time_list = tunnels[c][t][1][q]
+                    time_list[last_event_hour] += last_event_seconds
+                    for time in range(last_event_hour + 1, int(time_range * 3600 / period)):
+                        time_list[time] = period
+    return tunnels
+
 
 if __name__ == '__main__':
 
@@ -43,6 +143,7 @@ if __name__ == '__main__':
     entered_time = f'0:00 {now.month}/{now.day}/{now.year}'
     period_choice = '1'
     sys_ip = '10.1.1.1'
+    csv_file = None
 
     vmanage = v.vmanage_login()
 
@@ -51,20 +152,29 @@ if __name__ == '__main__':
         if sys_ip == 'q':
             vmanage.logout()
             exit(0)
-        time_range = int(input(f'Enter number of hours to display [{time_range}]: ') or 3)
+        time_range = int(input(f'Enter number of hours to display [{time_range}]: ') or time_range)
         last_N = input(f'Show "last N" hours? Enter "n" to enter a start time [{last_N}]: ') or last_N
+        if last_N == 'y':
+            last_n = True
         if last_N == 'n':
             last_n = False
             entered_time = input(f'Enter start time and date using 24-hour time in the format [{entered_time}]: ') or entered_time
             start_time = datetime.timestamp(datetime.strptime(entered_time, '%H:%M %m/%d/%Y'))
+        start_time = datetime.timestamp(datetime.strptime(entered_time, '%H:%M %m/%d/%Y'))
         period_choice = input(f'Enter period choice\n1: Hour\n2: 30 minutes\n3: 15 minutes\n[{period_choice}]? ') or period_choice
+        if (last_n):
+            tm = datetime.fromtimestamp(datetime.now().timestamp() - time_range * 3600)
+        else:
+            tm = datetime.strptime(entered_time, '%H:%M %m/%d/%Y')
+        file_time = f'{tm.year}{tm.month:02}{tm.day:02}{tm.hour:02}{tm.minute:02}'
+        csv_file = input(f'Enter csv file if desired [{sys_ip}-{file_time}-{time_range}hr.csv]: ') or f'{sys_ip}-{file_time}-{time_range}hr.csv'
         period = {'1': 3600, '2': 1800, '3': 900}[period_choice]
 
         device = vmanage.get_request(f'/device?deviceId={sys_ip}')
         site_id = device['data'][0]['site-id']
 
         # Get Events
-        sla_events = v.get_events(vmanage, last_n=last_n, hours=time_range, start_time=start_time, event_name='sla-change')
+        sla_events = v.get_events(vmanage, last_n=last_n, hours=time_range, start_time=start_time + gmt_adjust, event_name='sla-change')
         if last_n:
             start_time = int(sla_events['header']['generatedOn']/1000) - time_range * 3600
         sla_events = sla_events['data']
@@ -78,80 +188,9 @@ if __name__ == '__main__':
         tunnels = v.get_tunnels(vmanage, site=site_id)
         queues = v.get_queues(vmanage, sys_ip=sys_ip)
 
-        for color in tunnels:
-            for rip in tunnels[color]:
-                q = [0, {q: [0 for x in range(int(time_range*3600/period))] for q in queues}]
-                tunnels[color][rip] = q
+        tunnels = calculate_downtime(start_time, time_range, period, sla_events, tunnels, queues)
 
-        # parse events
-        for event in sla_events:
+        generate_output(tunnels, csv_file=csv_file)
 
-            e = event['event']
-
-            old_q = e['old-sla-classes'].split(', ')
-            new_q = e['sla-classes'].split(', ')
-
-            event_tunnel = tunnels[e['local-color']][e['remote-system-ip']]
-
-            event_time = int(event['entry_time'] / 1000) - start_time
-            if event_time == time_range * 3600:
-                event_time -= 1
-            event_hour = int(event_time / period)
-            event_seconds = event_time % period
-            tunnel_last_event_time = event_tunnel[0]
-            tunnel_last_event_hour = int(tunnel_last_event_time / period)
-            event_tunnel[0] = event_time
-
-            if tunnel_last_event_time == 0:
-                # first event on this tunnel
-                if old_q != ['None']:
-                    for queue in old_q:
-                        # for queues that were in SLA
-                        time_list = event_tunnel[1][queue]
-                        for t in range(event_hour):
-                            # fill in all fully elapsed hours
-                            time_list[t] = period
-                        # put remaining time in next hour
-                        time_list[event_hour] = event_seconds
-                event_tunnel.append(new_q)
-            else:
-                # later event on this tunnel
-                if old_q != ['None']:
-                    for queue in old_q:
-                        # if queue was in SLA, credit it for elapsed time
-                        time_list = event_tunnel[1][queue]
-                        if tunnel_last_event_hour == event_hour:
-                            time_list[event_hour] += event_time - tunnel_last_event_time
-                        else:
-                            time_list[tunnel_last_event_hour] += (tunnel_last_event_hour + 1) * period - tunnel_last_event_time
-                            for t in range(tunnel_last_event_hour + 1, event_hour):
-                                # fill in all fully elapsed hours
-                                time_list[t] = period
-                        # put remaining time in next hour
-                        time_list[event_hour] = event_seconds
-                event_tunnel[2] = new_q
-
-        # Fill in remaining time for Queues in SLA on last event
-        for c in tunnels:
-            for t in tunnels[c]:
-                last_event_time = tunnels[c][t][0]
-                last_event_hour = int(last_event_time / period)
-                # Calculate remainder seconds.  If event happened on an hour boundry seconds should be 3600.
-                last_event_seconds = (time_range * 3600 - last_event_time) % period or period
-                try:
-                    # Get queues in SLA on last time period
-                    new_q = tunnels[c][t][2]
-                except IndexError:
-                    # If there's no entry, there were no events. Assume all queues were in SLA
-                    new_q = queues
-                if new_q != ['None']:
-                    for q in new_q:
-                        time_list = tunnels[c][t][1][q]
-                        time_list[last_event_hour] += last_event_seconds
-                        for time in range(last_event_hour + 1, int(time_range*3600/period)):
-                            time_list[time] = period
-
-        print_queues(tunnels)
-
-        # print_tunnel_events(color = 'public-internet', rip = '10.46.16.55')
+        # print_tunnel_events(color = 'private2', rip='10.46.18.56')
 
